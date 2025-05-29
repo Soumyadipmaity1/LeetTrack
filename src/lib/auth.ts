0efd306cc112a16ba07daa8207b25e7c7c75af33
JagTheFriend <JagTheFriend12@gmail.com>
2025-05-29 13:02:29 +0530

    Added google authentication
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,10 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
 import { db } from "./db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(db),
   // To be determined later
-  providers: [],
+  providers: [Google],
 });
