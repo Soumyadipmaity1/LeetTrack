78e7a91c35548e83c454ee65960dd493f708ae29
JagTheFriend <JagTheFriend12@gmail.com>
2025-05-30 23:23:45 +0530

    Created a base layout
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

---
+++ src/Provider/BaseLayout.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { SidebarProvider } from "@/components/ui/sidebar";
+import Navbar from "@components/Essentials/Navbar";
+import { SSidebar } from "@components/Essentials/SSidebar";
+
+export default function BaseLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <SidebarProvider>
+      <div className="flex h-screen">
+        <div className="w-64">
+          <SSidebar />
+        </div>
+        <div className="flex-1 flex flex-col">
+          <Navbar />
+          <main className="flex-1 p-6 overflow-y-auto bg-white">
+            {children}
+          </main>
+        </div>
+      </div>
+    </SidebarProvider>
+  );
+}
