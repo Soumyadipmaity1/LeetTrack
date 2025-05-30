d2c8e7fe84495f0e54fd56b64d03a1fb4f75fb18
JagTheFriend <JagTheFriend12@gmail.com>
2025-05-30 23:33:58 +0530

    Use the BaseLayout provider to provide navbar and sidebar
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,25 +1,7 @@
-import { SidebarProvider } from "@/components/ui/sidebar";
-import { SSidebar } from "@components/Essentials/SSidebar";
-import Navbar from "@components/Essentials/Navbar";
-
-export default function AdminLayout({children}:{children: React.ReactNode}){
-  return(
-    <SidebarProvider>
-      <div className="flex h-screen">
-        <div className="w-64">
-        <SSidebar />
-        </div>
-      
-      
-      <div className="flex-1 flex flex-col">
-      <Navbar />
-      
-      <main className="flex-1 p-6 overflow-y-auto bg-white">
-        
-        {children}
-      </main>
-      </div>
-      </div>
-    </SidebarProvider>
-  )
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
 }
