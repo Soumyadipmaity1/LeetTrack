1bcaac762b4c9a489e43b710cb71919f501aa8b1
JagTheFriend <JagTheFriend12@gmail.com>
2025-05-30 23:33:29 +0530

    Make the cursor a pointer when user hovers over it
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/components/Essentials/SSidebar.tsx
+++ src/components/Essentials/SSidebar.tsx
@@ -1,65 +1,50 @@
-import{
+import {
   Sidebar,
   SidebarContent,
-  SidebarMenuItem,
-  SidebarFooter,
-  SidebarMenu,
-  SidebarHeader,
   SidebarGroup,
   SidebarGroupContent,
-  SidebarMenuButton,
   SidebarGroupLabel,
-}from "@/components/ui/sidebar"
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
 
 const elements = [
-
   {
     title: "Dashboard",
     url: "#",
-    
   },
   {
     title: "Calendar",
     url: "#",
-    
   },
   {
     title: "Settings",
     url: "#",
-   
   },
-
-]
-
-
-
+];
 
 export function SSidebar() {
   return (
     <Sidebar className="pt-10 bg-gray-800">
       <SidebarContent>
-
-      <SidebarGroup>
-          
+        <SidebarGroup>
           <SidebarGroupLabel></SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
-             {elements.map((ele)=>(
-              <SidebarMenuItem key={ele.title}>
-                <SidebarMenuButton asChild>
-                <a href={ele.url}>
-                
-                <span>{ele.title}</span>
-                </a>
-                </SidebarMenuButton>
-
-              </SidebarMenuItem>
-             ))}
+              {elements.map((ele) => (
+                <SidebarMenuItem key={ele.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={ele.url}>
+                      <span>{ele.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
-        </SidebarContent>
+      </SidebarContent>
     </Sidebar>
-  )
+  );
 }
-
