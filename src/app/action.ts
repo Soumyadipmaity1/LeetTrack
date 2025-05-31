2383de5217dc5cba41bf81d5af6c96236a1ba414
JagTheFriend <JagTheFriend12@gmail.com>
2025-05-31 11:47:48 +0530

    Implement create and update reminder functionalities with appropriate schemas
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/action.ts
+++ src/app/action.ts
@@ -4,32 +4,6 @@ import { searchQuestion } from "@lib/leetcode";
 import { authActionClient } from "@lib/safe-action";
 import { z } from "zod";
 
-// Used for fetching reminders for the authenticated user.
-export const getReminders = authActionClient.action(async ({ ctx }) => {
-  // Note: You can check the status of each reminder based on REMINDER_STATUS enum
-  // and filter them accordingly if needed (refer to prisma schema for details).
-  return await ctx.db.reminder.findMany({
-    where: {
-      userId: ctx.user.id,
-    },
-  });
-});
-
-const deleteReminderSchema = z.object({
-  reminderId: z.string().cuid(),
-});
-// Used for deleting a reminder by its ID.
-export const deleteReminder = authActionClient
-  .schema(deleteReminderSchema)
-  .action(async ({ ctx, parsedInput }) => {
-    const { reminderId } = parsedInput;
-    return await ctx.db.reminder.delete({
-      where: {
-        id: reminderId,
-      },
-    });
-  });
-
 const searchQuestionsSchema = z.object({
   questionTitle: z.string().optional(),
   questionTags: z.array(z.string()).optional(),
@@ -44,5 +18,79 @@ export const searchQuestions = authActionClient
       questionTitle: parsedInput.questionTitle,
       questionTags: parsedInput.questionTags,
       questionDifficulty: parsedInput.questionDifficulty,
+    });
+  });
+
+const createReminderSchema = z.object({
+  problemId: z.string().optional(),
+  problemName: z.string().optional(),
+  problemStatement: z.string().optional(),
+  problemTags: z.array(z.string()).optional(),
+  problemDifficulty: z.enum(["EASY", "MEDIUM", "HARD"]).optional(),
+  problemLink: z.string(),
+  scheduledDate: z.date(),
+});
+
+// Used for creating a reminder.
+export const createReminder = authActionClient
+  .schema(createReminderSchema)
+  .action(async ({ ctx, parsedInput }) => {
+    await ctx.db.reminder.create({
+      data: {
+        userId: ctx.user.id ?? "user_id",
+        ...parsedInput,
+      },
+    });
+  });
+
+// Used for fetching reminders for the authenticated user.
+export const getReminders = authActionClient.action(async ({ ctx }) => {
+  // Note: You can check the status of each reminder based on REMINDER_STATUS enum
+  // and filter them accordingly if needed (refer to prisma schema for details).
+  return await ctx.db.reminder.findMany({
+    where: {
+      userId: ctx.user.id,
+    },
+  });
+});
+
+const updateReminderSchema = z.object({
+  reminderId: z.string().cuid(),
+  problemId: z.string().optional(),
+  problemName: z.string().optional(),
+  problemStatement: z.string().optional(),
+  problemTags: z.array(z.string()).optional(),
+  problemDifficulty: z.enum(["EASY", "MEDIUM", "HARD"]).optional(),
+  problemLink: z.string(),
+  scheduledDate: z.date(),
+});
+
+// Used for updating a reminder.
+export const updateReminder = authActionClient
+  .schema(updateReminderSchema)
+  .action(async ({ ctx, parsedInput }) => {
+    await ctx.db.reminder.update({
+      where: {
+        id: parsedInput.reminderId,
+      },
+      data: {
+        userId: ctx.user.id ?? "user_id",
+        ...parsedInput,
+      },
+    });
+  });
+
+const deleteReminderSchema = z.object({
+  reminderId: z.string().cuid(),
+});
+// Used for deleting a reminder by its ID.
+export const deleteReminder = authActionClient
+  .schema(deleteReminderSchema)
+  .action(async ({ ctx, parsedInput }) => {
+    const { reminderId } = parsedInput;
+    return await ctx.db.reminder.delete({
+      where: {
+        id: reminderId,
+      },
     });
   });
