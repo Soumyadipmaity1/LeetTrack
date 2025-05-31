301fa2c7e819d43f72acfef74c349f9d98c80a42
JagTheFriend <JagTheFriend12@gmail.com>
2025-05-31 11:37:47 +0530

    Refactor action.ts to include deleteReminder and searchQuestions functionalities with appropriate schemas
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/action.ts
+++ src/app/action.ts
@@ -1,4 +1,8 @@
+"use server";
+
+import { searchQuestion } from "@lib/leetcode";
 import { authActionClient } from "@lib/safe-action";
+import { z } from "zod";
 
 // Used for fetching reminders for the authenticated user.
 export const getReminders = authActionClient.action(async ({ ctx }) => {
@@ -10,3 +14,35 @@ export const getReminders = authActionClient.action(async ({ ctx }) => {
     },
   });
 });
+
+const deleteReminderSchema = z.object({
+  reminderId: z.string().cuid(),
+});
+// Used for deleting a reminder by its ID.
+export const deleteReminder = authActionClient
+  .schema(deleteReminderSchema)
+  .action(async ({ ctx, parsedInput }) => {
+    const { reminderId } = parsedInput;
+    return await ctx.db.reminder.delete({
+      where: {
+        id: reminderId,
+      },
+    });
+  });
+
+const searchQuestionsSchema = z.object({
+  questionTitle: z.string().optional(),
+  questionTags: z.array(z.string()).optional(),
+  questionDifficulty: z.enum(["EASY", "MEDIUM", "HARD"]).optional(),
+});
+
+// Fetching details of leetcode question
+export const searchQuestions = authActionClient
+  .schema(searchQuestionsSchema)
+  .action(async ({ parsedInput }) => {
+    return await searchQuestion({
+      questionTitle: parsedInput.questionTitle,
+      questionTags: parsedInput.questionTags,
+      questionDifficulty: parsedInput.questionDifficulty,
+    });
+  });
