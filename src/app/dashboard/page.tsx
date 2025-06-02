f6d28b740eae3a5cc584314dc0aabcd9e6cdb47d
Diya Rao <diyarao0202@gmail.com>
2025-06-02 11:46:11 +0530

    Added listview button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,13 +2,18 @@
 
 import React from "react"
 import SearchFilter from "@/components/Essentials/SearchFilter"
-
+import { Button } from "@/components/ui/button"
 
 const Page = () => {
   return (
     <div className="flex justify-center items-start min-h-screen px-4 pt-32">
       <div className="w-full max-w-5xl space-y-6">
-       <SearchFilter />
+        <div>
+          <Button variant="outline" size="sm" className="gap-2">
+            List View
+          </Button>
+        </div>
+        <SearchFilter />
       </div>
     </div>
   )
