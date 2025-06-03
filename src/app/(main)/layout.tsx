3b5d8cd9a01b049d8ca5c3a6331503e72a8f4c6a
JagTheFriend <JagTheFriend12@gmail.com>
2025-06-03 20:13:32 +0530

    Fixed import error
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,7 +1,6 @@
 import BaseLayout from "@/Provider/BaseLayout";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,9 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-       
-          <BaseLayout>{children}</BaseLayout>
-        
+        <BaseLayout>{children}</BaseLayout>
       </body>
     </html>
   );
