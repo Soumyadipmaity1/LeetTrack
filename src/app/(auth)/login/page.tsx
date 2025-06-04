44f9305b3f6ec22fd35032173e720ad37537c540
JagTheFriend <JagTheFriend12@gmail.com>
2025-06-04 12:40:24 +0530

    Added some styling
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,8 +4,11 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function LoginPage() {
   return (
-    <>
+    <div
+      className="flex justify-center p-10 bg-gray-700"
+      style={{ height: "100vh" }}
+    >
       <SignIn routing="hash" />
-    </>
+    </div>
   );
 }
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -3,8 +3,8 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
   return (
-    <>
+    <div className="flex justify-center p-10 bg-gray-700">
       <SignUp routing="hash" />
-    </>
+    </div>
   );
 }
