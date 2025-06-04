0cac08c4933e6c4056d05b02b446098fd0bc75c2
JagTheFriend <JagTheFriend12@gmail.com>
2025-06-04 11:36:29 +0530

    Remove auth check for now
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,17 +1,15 @@
 import BaseLayout from "@/Provider/BaseLayout";
-import { auth } from "@lib/auth";
-import { redirect } from "next/navigation";
 
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const data = await auth();
+  // const data = await auth();
 
-  if (!data?.user) {
-    redirect("/login");
-  }
+  // if (!data?.user) {
+  //   redirect("/login");
+  // }
 
   return <BaseLayout>{children}</BaseLayout>;
 }
