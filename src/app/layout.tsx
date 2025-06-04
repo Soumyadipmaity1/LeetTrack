6b400a072224ca5303e42bb6e3af1b4ccf29102b
JagTheFriend <JagTheFriend12@gmail.com>
2025-06-04 11:05:51 +0530

    Added toast provider
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { SessionProvider } from "next-auth/react";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Toaster } from "sonner";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SessionProvider>{children}</SessionProvider>
+        <SessionProvider>
+          <Toaster richColors />
+          {children}
+        </SessionProvider>
       </body>
     </html>
   );
