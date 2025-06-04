5aede1ca52f7458d7954bf32ac78a62379c57657
JagTheFriend <JagTheFriend12@gmail.com>
2025-06-04 14:22:54 +0530

    Display user profile
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/Provider/BaseLayout.tsx
+++ src/Provider/BaseLayout.tsx
@@ -11,21 +11,18 @@ export default function BaseLayout({
 }) {
   return (
     <SidebarProvider>
-
-    <div className="flex h-screen w-full">
-
-      <div className="flex h-screen w-screen">
-
-        <div className="w-64">
-          <SSidebar />
+      <div className="flex h-screen w-full">
+        <div className="flex h-screen w-screen">
+          <div className="w-64">
+            <SSidebar />
+          </div>
+          <div className="flex-1 flex flex-col">
+            <Navbar />
+            <main className="flex-1 p-6 overflow-y-auto bg-white">
+              {children}
+            </main>
+          </div>
         </div>
-        <div className="flex-1 flex flex-col">
-          <Navbar />
-          <main className="flex-1 p-6 overflow-y-auto bg-white">
-            {children}
-          </main>
-        </div>
-      </div>
       </div>
     </SidebarProvider>
   );
