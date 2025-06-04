3b3049a1b4bc25da1529f772f11cdee274b20336
JagTheFriend <JagTheFriend12@gmail.com>
2025-06-04 11:05:38 +0530

    Use sonner to display errors
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/(auth)/_components/credentialsForm.tsx
+++ src/app/(auth)/_components/credentialsForm.tsx
@@ -1,7 +1,7 @@
 import { signUp } from "@/app/(auth)/signup/action";
 import { signIn } from "next-auth/react";
 import { redirect } from "next/navigation";
-import { useState } from "react";
+import { toast } from "sonner";
 import { Button } from "../../../components/ui/button";
 
 interface CredentialsFormProps {
@@ -10,8 +10,6 @@ interface CredentialsFormProps {
 }
 
 export function CredentialsForm(props: CredentialsFormProps) {
-  const [error, setError] = useState<string | null>(null);
-
   const handleSubmit = async (data: FormData) => {
     switch (props.formType) {
       case "login":
@@ -21,10 +19,11 @@ export function CredentialsForm(props: CredentialsFormProps) {
           redirect: false,
         });
 
-        if (signInResponse && !signInResponse.error) {
+        console.log(signInResponse);
+        if (signInResponse && !signInResponse.error && signInResponse.ok) {
           redirect("/dashboard");
         } else {
-          setError("Your Email or Password is wrong!");
+          toast.error("Your Email or Password is wrong!");
         }
         break;
 
@@ -34,9 +33,9 @@ export function CredentialsForm(props: CredentialsFormProps) {
           (data.get("password") as string) ?? ""
         );
         if (signUpResponse) {
-          redirect("/dashboard"); // Change to your desired route
+          redirect("/dashboard");
         } else {
-          setError("Something went wrong!. Please try again later.");
+          toast.error("Something went wrong!. Please try again later.");
         }
     }
   };
@@ -48,11 +47,6 @@ export function CredentialsForm(props: CredentialsFormProps) {
         await handleSubmit(formData);
       }}
     >
-      {error && (
-        <span className="p-4 mb-2 text-lg font-semibold text-white bg-red-500 rounded-md text-center">
-          {error}
-        </span>
-      )}
       <label htmlFor="email" className="mb-1 font-semibold text-sm">
         Email
       </label>
