34f1a4d3036c33bef0059d013f37b2846d60c9ae
JagTheFriend <JagTheFriend12@gmail.com>
2025-06-04 13:48:18 +0530

    Set login and signup as public
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/api/webhook/(.*)"]);
+const isPublicRoute = createRouteMatcher([
+  "/api/webhook/(.*)",
+  "/login",
+  "/signup",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
