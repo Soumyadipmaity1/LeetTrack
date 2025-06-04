b0a5d4edc927676464346d9b293c927cbc37d0b5
JagTheFriend <JagTheFriend12@gmail.com>
2025-06-04 12:07:04 +0530

    Make the cursor pointer when hovering over google button
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/(auth)/_components/GoogleSignInButton.tsx
+++ src/app/(auth)/_components/GoogleSignInButton.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import Image from "next/image";
-import googleLogo from "@/public/google.png";
 import { signIn } from "next-auth/react";
+import Image from "next/image";
 
 export function GoogleSignInButton() {
   const handleClick = () => {
@@ -12,7 +11,7 @@ export function GoogleSignInButton() {
   return (
     <button
       onClick={handleClick}
-      className="w-full flex items-center font-semibold justify-center h-14 px-6 mt-4 text-xl transition-colors duration-300 bg-white border-2 border-black text-black rounded-lg focus:shadow-outline hover:bg-slate-200"
+      className="w-full flex items-center font-semibold justify-center h-14 px-6 mt-4 text-xl transition-colors duration-300 bg-white border-2 border-black text-black rounded-lg focus:shadow-outline hover:bg-slate-200 cursor-pointer"
     >
       <Image src="/google.png" alt="Google Logo" width={20} height={20} />
       <span className="ml-4">Continue with Google</span>
