b1e56588eac54de750138b819396c1cebbf5c49a
JagTheFriend <JagTheFriend12@gmail.com>
2025-06-04 10:22:24 +0530

    Implement user retrieval and password verification functions
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,28 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import crypto from "crypto";
+import { twMerge } from "tailwind-merge";
+import { db } from "./db";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getUserFromDb(email: string) {
+  return db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+}
+
+export async function verifyPassword(
+  givenPassword: string,
+  passwordHash: string,
+  passwordSalt: string
+) {
+  const givenPasswordHash = crypto
+    .pbkdf2Sync(givenPassword, passwordSalt, 10_000, 64, "sha512")
+    .toString("hex");
+
+  return givenPasswordHash === passwordHash;
 }
