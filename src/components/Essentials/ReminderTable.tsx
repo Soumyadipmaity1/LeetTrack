21a09316f1d9cd639e57319c770c1f973681b3fb
JagTheFriend <JagTheFriend12@gmail.com>
2025-06-06 09:07:48 +0530

    Working on context menu
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/components/Essentials/ReminderTable.tsx
+++ src/components/Essentials/ReminderTable.tsx
@@ -10,45 +10,32 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Badge } from "@components/ui/badge";
+import {
+  ContextMenu,
+  ContextMenuCheckboxItem,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuLabel,
+  ContextMenuRadioGroup,
+  ContextMenuRadioItem,
+  ContextMenuSeparator,
+  ContextMenuShortcut,
+  ContextMenuSub,
+  ContextMenuSubContent,
+  ContextMenuSubTrigger,
+  ContextMenuTrigger,
+} from "@components/ui/context-menu";
 import { cn } from "@lib/utils";
 import { PROBLEM_DIFFICULTY, Reminder, REMINDER_STATUS } from "@prisma-client";
 import { SquareArrowOutUpRight } from "lucide-react";
 import Link from "next/link";
+import { useCallback } from "react";
 
 export default function RemainderTable({
   reminders,
 }: {
   reminders: Reminder[];
 }) {
-  const getDifficultyBadge = (level: PROBLEM_DIFFICULTY) => {
-    const variant = {
-      EASY: "bg-green-100 text-green-600",
-      MEDIUM: "bg-yellow-100 text-yellow-600",
-      HARD: "bg-red-100 text-red-600",
-    } as Record<PROBLEM_DIFFICULTY, string>;
-    return (
-      <span
-        className={`px-2 py-1 rounded-md text-xs font-medium ${variant[level]}`}
-      >
-        {level}
-      </span>
-    );
-  };
-
-  const getStatusBadge = (status: keyof typeof REMINDER_STATUS) => {
-    const bgStyle =
-      status === "COMPLETED"
-        ? "bg-green-500"
-        : status === "PENDING"
-        ? "bg-red-500"
-        : "bg-gray-600";
-    return (
-      <Badge variant={"secondary"} className={cn("text-white", bgStyle)}>
-        {status}
-      </Badge>
-    );
-  };
-
   return (
     <div className="border p-4 rounded">
       <h2 className="text-xl font-bold">Your Reminders</h2>
@@ -68,45 +55,127 @@ export default function RemainderTable({
             </TableHeader>
             <TableBody className="text-base cursor-default">
               {reminders.map((reminder, index) => (
-                <TableRow
-                  className="cursor-pointer"
-                  key={index + Math.random()}
-                  onClick={() => {
-                    window.open(
-                      `https://leetcode.com/problems/${reminder.problemName}`,
-                      "_blank"
-                    );
-                  }}
-                >
-                  <TableCell>
-                    <Link
-                      className="flex flex-row gap-2 font-semibold hover:underline"
-                      href={`https://leetcode.com/problems/${reminder.problemName}`}
-                      target="_blank"
-                    >
-                      {reminder.problemName}
-                      <SquareArrowOutUpRight size={10} className="mt-2" />
-                    </Link>
-                  </TableCell>
-                  <TableCell>
-                    {getDifficultyBadge(reminder.problemDifficulty)}
-                  </TableCell>
-                  <TableCell>
-                    {reminder.scheduledDate.toLocaleDateString("en-US", {
-                      year: "numeric",
-                      month: "short",
-                      day: "numeric",
-                    })}
-                  </TableCell>
-                  <TableCell>
-                    {getStatusBadge(reminder.reminderStatus)}
-                  </TableCell>
-                </TableRow>
+                <CustomContextMenu>
+                  <CustomTableRow
+                    reminder={reminder}
+                    key={index + Math.random()}
+                  />
+                </CustomContextMenu>
               ))}
             </TableBody>
           </Table>
         </CardContent>
       </Card>
     </div>
+  );
+}
+
+function CustomTableRow({ reminder }: { reminder: Reminder }) {
+  const getDifficultyBadge = useCallback((level: PROBLEM_DIFFICULTY) => {
+    const variant = {
+      EASY: "bg-green-100 text-green-600",
+      MEDIUM: "bg-yellow-100 text-yellow-600",
+      HARD: "bg-red-100 text-red-600",
+    } as Record<PROBLEM_DIFFICULTY, string>;
+    return (
+      <span
+        className={`px-2 py-1 rounded-md text-xs font-medium ${variant[level]}`}
+      >
+        {level}
+      </span>
+    );
+  }, []);
+
+  const getStatusBadge = useCallback((status: keyof typeof REMINDER_STATUS) => {
+    const bgStyle =
+      status === "COMPLETED"
+        ? "bg-green-500"
+        : status === "PENDING"
+        ? "bg-red-500"
+        : "bg-gray-600";
+    return (
+      <Badge variant={"secondary"} className={cn("text-white", bgStyle)}>
+        {status}
+      </Badge>
+    );
+  }, []);
+
+  return (
+    <TableRow
+      className="cursor-pointer"
+      onClick={() => {
+        window.open(
+          `https://leetcode.com/problems/${reminder.problemName}`,
+          "_blank"
+        );
+      }}
+    >
+      <TableCell>
+        <Link
+          className="flex flex-row gap-2 font-semibold hover:underline"
+          href={`https://leetcode.com/problems/${reminder.problemName}`}
+          target="_blank"
+        >
+          {reminder.problemName}
+          <SquareArrowOutUpRight size={10} className="mt-2" />
+        </Link>
+      </TableCell>
+      <TableCell>{getDifficultyBadge(reminder.problemDifficulty)}</TableCell>
+      <TableCell>
+        {reminder.scheduledDate.toLocaleDateString("en-US", {
+          year: "numeric",
+          month: "short",
+          day: "numeric",
+        })}
+      </TableCell>
+      <TableCell>{getStatusBadge(reminder.reminderStatus)}</TableCell>
+    </TableRow>
+  );
+}
+
+function CustomContextMenu({ children }: { children: React.ReactNode }) {
+  return (
+    <ContextMenu>
+      <ContextMenuTrigger asChild>{children}</ContextMenuTrigger>
+      <ContextMenuContent className="w-52">
+        <ContextMenuItem inset>
+          Back
+          <ContextMenuShortcut>⌘[</ContextMenuShortcut>
+        </ContextMenuItem>
+        <ContextMenuItem inset disabled>
+          Forward
+          <ContextMenuShortcut>⌘]</ContextMenuShortcut>
+        </ContextMenuItem>
+        <ContextMenuItem inset>
+          Reload
+          <ContextMenuShortcut>⌘R</ContextMenuShortcut>
+        </ContextMenuItem>
+        <ContextMenuSub>
+          <ContextMenuSubTrigger inset>More Tools</ContextMenuSubTrigger>
+          <ContextMenuSubContent className="w-44">
+            <ContextMenuItem>Save Page...</ContextMenuItem>
+            <ContextMenuItem>Create Shortcut...</ContextMenuItem>
+            <ContextMenuItem>Name Window...</ContextMenuItem>
+            <ContextMenuSeparator />
+            <ContextMenuItem>Developer Tools</ContextMenuItem>
+            <ContextMenuSeparator />
+            <ContextMenuItem variant="destructive">Delete</ContextMenuItem>
+          </ContextMenuSubContent>
+        </ContextMenuSub>
+        <ContextMenuSeparator />
+        <ContextMenuCheckboxItem checked>
+          Show Bookmarks
+        </ContextMenuCheckboxItem>
+        <ContextMenuCheckboxItem>Show Full URLs</ContextMenuCheckboxItem>
+        <ContextMenuSeparator />
+        <ContextMenuRadioGroup value="pedro">
+          <ContextMenuLabel inset>People</ContextMenuLabel>
+          <ContextMenuRadioItem value="pedro">
+            Pedro Duarte
+          </ContextMenuRadioItem>
+          <ContextMenuRadioItem value="colm">Colm Tuite</ContextMenuRadioItem>
+        </ContextMenuRadioGroup>
+      </ContextMenuContent>
+    </ContextMenu>
   );
 }
