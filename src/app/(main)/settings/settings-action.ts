c8d6b3c2b9c594f423c16206261c3fb674030c62
JagTheFriend <JagTheFriend12@gmail.com>
2025-06-06 12:17:23 +0530

    Added action to get user data
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/(main)/settings/settings-action.ts
+++ src/app/(main)/settings/settings-action.ts
@@ -4,21 +4,6 @@ import { authActionClient } from "@lib/safe-action";
 import { NOTIFICATION_METHOD } from "@prisma-client";
 import z from "zod";
 
-const updateUserProfileSchema = z.object({
-  leetCodeUsername: z.string().optional(),
-  bio: z.string().optional(),
-  goals: z.string().optional(),
-});
-
-export const updateUserProfile = authActionClient
-  .schema(updateUserProfileSchema)
-  .action(async ({ ctx, parsedInput }) => {
-    return await ctx.db.user.update({
-      where: { id: ctx.user.userId },
-      data: { ...parsedInput },
-    });
-  });
-
 const updateNotificationSettingsSchema = z.object({
   preferredNotificationMethod: z.nativeEnum(NOTIFICATION_METHOD),
 });
@@ -27,7 +12,13 @@ export const updateNotificationSettings = authActionClient
   .schema(updateNotificationSettingsSchema)
   .action(async ({ ctx, parsedInput }) => {
     return await ctx.db.user.update({
-      where: { id: ctx.user.userId },
+      where: { id: ctx.user.id },
       data: { ...parsedInput },
     });
   });
+
+export const getSettings = authActionClient.action(async ({ ctx }) => {
+  return await ctx.db.user.findUnique({
+    where: { id: ctx.user.id },
+  });
+});
