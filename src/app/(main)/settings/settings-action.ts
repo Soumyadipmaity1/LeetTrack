2a8a7bac622b7d6713a5622f645ed066a067cbb8
JagTheFriend <JagTheFriend12@gmail.com>
2025-06-08 09:21:16 +0530

    Added action for updating email notifications
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/(main)/settings/settings-action.ts
+++ src/app/(main)/settings/settings-action.ts
@@ -1,24 +1,26 @@
 "use server";
 
 import { authActionClient } from "@lib/safe-action";
-import { NOTIFICATION_METHOD } from "@prisma-client";
 import z from "zod";
-
-const updateNotificationSettingsSchema = z.object({
-  preferredNotificationMethod: z.nativeEnum(NOTIFICATION_METHOD),
-});
-
-export const updateNotificationSettings = authActionClient
-  .schema(updateNotificationSettingsSchema)
-  .action(async ({ ctx, parsedInput }) => {
-    return await ctx.db.user.update({
-      where: { externalUserId: ctx.user.externalUserId },
-      data: { ...parsedInput },
-    });
-  });
 
 export const getSettings = authActionClient.action(async ({ ctx }) => {
   return await ctx.db.user.findUnique({
     where: { externalUserId: ctx.user.externalUserId },
   });
 });
+
+const updateEmailNotificationSettingsSchema = z.object({
+  sendEmailReminder: z.boolean(),
+  sendUpcomingReminder: z.boolean(),
+  sendDailyDigest: z.boolean(),
+  sendWeeklyReport: z.boolean(),
+});
+
+export const updateEmailNotificationSettings = authActionClient
+  .schema(updateEmailNotificationSettingsSchema)
+  .action(async ({ ctx, parsedInput }) => {
+    return await ctx.db.user.update({
+      where: { externalUserId: ctx.user.externalUserId },
+      data: parsedInput,
+    });
+  });
