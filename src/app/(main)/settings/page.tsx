6c92defc804ceb4a9f3f0673bbb3b33fc2044b0b
JagTheFriend <JagTheFriend12@gmail.com>
2025-06-08 09:04:46 +0530

    No need to use tab
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/(main)/settings/page.tsx
+++ src/app/(main)/settings/page.tsx
@@ -1,12 +1,5 @@
 "use server";
 
-import { Roboto } from "next/font/google";
-const roboto = Roboto({
-  weight: "400",
-  subsets: ["latin"],
-});
-
-import { Tabs } from "@/components/ui/tabs";
 import EmailNotifications from "@components/Essentials/EmailNotification";
 import PushNotification from "@components/Essentials/PushNotification";
 import { getSettings } from "./settings-action";
@@ -21,11 +14,11 @@ export default async function SettingsPage() {
     <main className="min-h-screen flex flex-col items-center">
       <div className="w-full max-w-5xl">
         <h1 className="text-3xl font-bold mb-1">Settings</h1>
-        <Tabs defaultValue="profile" className="w-full">
+        <div className="w-full">
           {/* Notification page will be added here*/}
           <EmailNotifications userData={userData} />
           <PushNotification userData={userData} />
-        </Tabs>
+        </div>
       </div>
     </main>
   );
